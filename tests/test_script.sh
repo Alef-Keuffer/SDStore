88a3b55147b22554d8336f659c862268ecd237a3
Alef-Keuffer <alefkeuffer@gmail.com>
2022-05-14 16:47:02 +0100

    Automatic testing using cmake

--- tests/test_script.sh
+++ tests/test_script.sh
@@ -1,28 +1,5 @@
 #!/bin/bash
 
-# Move to project root
-cd ..
-
-# Build SDStore executables, and return to project root.
-# Using subshell to avoid having to cd ..
-# https://www.shellcheck.net/wiki/SC2103
-(
-    cd bin/sdstore-transformations || return
-    make
-)
-
-
-# Build the project executables, and return to project's root.
-# Subshell, same as above.
-(
-    cd build || exit
-    cmake ..;
-    make;
-)
-
-# Move to test directory to avoid cluttering root of project with files.
-cd tests || return
-
 # How many test files to generate, both input and output.
 num_files=5
 
