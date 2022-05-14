9aa5a95e036a86436a5b8b9931ec074d74ae2c69
Alexandre Baldé <alexandrer_b@outlook.com>
2022-05-14 17:58:36 +0100

    Separate building and testing into two scripts

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Build SDStore executables, and return to project root.
+# Using subshell to avoid having to cd ..
+# https://www.shellcheck.net/wiki/SC2103
+(
+    cd bin/sdstore-transformations || return;
+    make clean;
+    make;
+)
+
+# Build the project executables, and return to project's root.
+# Subshell, same as above.
+cmake --build ./build --config Release --target all -j 18 --
+
+cp build/server tests
+cp build/client tests
+
+cp ./etc/sdstored.conf tests/sdstored.conf
+
+mkdir -p tests/bin/sdstore-transformations/
+cp -r ./bin/sdstore-transformations/ tests/bin
