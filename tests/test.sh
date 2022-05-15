732ffd36a425f2f456f7f89aa25eaeadede12576
Alexandre Baldé (WSL Win11 Pro) <alexandrer_b@outlook.com>
2022-05-15 17:28:21 +0100

    Improve build and test scripts further; add more tmuxinator tests

--- tests/test.sh
+++ tests/test.sh
@@ -16,4 +16,17 @@ for ((i=1;i<=num_files;i++)); do
     touch fileout"$i";
 done
 
-tmuxinator start -p concurrency_and_limit.yml
+# Argument passed to bash script dictates what test to run
+if [ "$1" == "conc" ]; then
+    tmuxinator start -p concurrency.yml
+elif [ "$1" == "prio" ]; then
+    tmuxinator start -p priority.yml;
+elif [ "$1" == "status" ]; then
+    tmuxinator start -p status.yml;
+elif [ "$1" == "impossible" ]; then
+    tmuxinator start -p impossible.yml;
+elif [ "$1" == "ctrl_c" ]; then
+    tmuxinator start -p ctrl_c.yml;
+else
+    echo "Unknown test parameter! Please rerun with an appropriate argument."
+fi
