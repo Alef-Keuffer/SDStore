049c9b341f455baf22dabc60ca7577db3bd3bfd7
Alef-Keuffer <alefkeuffer@gmail.com>
2022-05-15 15:12:36 +0100

    finished diagram

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /*!
 
 @mainpage Example use
@@ -16,33 +17,29 @@ activate User
 
 == Initialization ==
 activate Admin
-Admin -> Server: ./sdstored configFilePath transformationsFolderPath
+Admin -> Server: ./server ⟨configFilePath⟩ ⟨transformationsFolderPath⟩
 deactivate Admin
 activate Server
 
 == proc-file ==
-User -> Client: ./sdstore proc-file priority inPath outPath [transformations]
+User -> Client: ./client proc-file ⟨priority⟩ ⟨src⟩ ⟨dst⟩ ⟨transformation⟩⁺
 activate Client
-Client -> Server: ?
-Server -> Client: ?
-Client -> User: pending
-Server -> Client: ?
-Client -> User: processing
-Server -> Client: ?
-Client -> User: finished + bytes read + bytes written
+Client -> Server: ⟨client_pid_str⟩ ⟨PROC_FILE⟩ ⟨priority⟩ ⟨src⟩ ⟨dst⟩ ⟨num_ops⟩ ⟨ops⟩⁺
+Server -> Client: pending
+Server -> Client: processing
+Server -> Client: finished + bytes read + bytes written
 deactivate Client
 
 == status ==
-User -> Client: ./sdstore status
+User -> Client: ./client status
 activate Client
-Client -> Server: ?
-Server -> Client: ?
-Client -> User: tasks + transformations
+Client -> Server: ⟨client_pid_str⟩ ⟨STATUS⟩
+Server -> Client: ⟨get_status_str()⟩
 deactivate Client
 
 == Termination ==
 
-Server <-? : SIGTERM
+Server <-? : SIGTERM or SIGINT
 deactivate Server
 
 
@@ -52,9 +49,13 @@ deactivate Server
 
 
 
-
+void test1(const char*format, ...) {
+  printf(format);
+}
 
 int main ()
 {
+  int i = 2;
+  test1("%s%d\n", "alef", i);
   return 0;
 }
